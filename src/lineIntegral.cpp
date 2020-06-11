76d6bc487ceb480959905fce786a471ac20b606b
liwanlin <liwanlin@mit.edu>
2020-06-10 23:29:07 -0400

    implement line integral from point

--- src/lineIntegral.cpp
+++ src/lineIntegral.cpp
@@ -1,11 +1,60 @@
 #include "lineIntegral.hpp"
 
-LineIntegral::LineIntegral() {}
 
-double LineIntegral::evaluate(double (*func)(double, double), Triangle *triangle) {
+// helper for extracting grayscale
 
+const double RED_LUMINANCE = 0.2126;
+const double GREEN_LUMINANCE = 0.7152;
+const double BLUE_LUMINANCE = 0.0722;
+
+// get the luminance at pixel (x, y) by standard luminance transformation
+int getLuminance(CImg<unsigned char> *img, int x, int y) {
+	int red_val = (int) (*img)(x, y, 0, 0);	
+	int green_val = (int) (*img)(x, y, 0, 1);
+	int blue_val = (int) (*img)(x, y, 0, 2);
+	return round(red_val * RED_LUMINANCE + green_val * GREEN_LUMINANCE + blue_val * BLUE_LUMINANCE);
 }
 
-double LineIntegral::evaluate(double (*func)(double, double), Point *a, Point *b, Point *c) {
+LineIntegral::LineIntegral() {}
 
+// double LineIntegral::evaluate(double (*func)(double, double), CImg<unsigned char> *img, Triangle *triangle) {
+double LineIntegral::evaluate(double (*func)(double, double), Triangle *triangle) {
+    int xMin, xMax, yMin, yMax;
+    // compute bounding box of triangle, in pixels
+    triangle->boundingBox(xMin, xMax, yMin, yMax);
+    // iterate over bounding box
+    double integral = 0;
+    for(int i = xMin; i <= xMax; i++) {
+        for(int j = yMin; j <= yMax; j++) {
+            Pixel p(i, j, 1);
+
+        }
+    }
+}
+
+// double LineIntegral::evaluate(double (*func)(double, double), CImg<unsigned char> *img, Point *a, Point *b, Point *c) {
+double LineIntegral::evaluate(double (*func)(double, double), Point *a, Point *b, Point *c) {
+    // first compute bounding box
+    double ax = a->getX();
+    double ay = a->getY();
+    double bx = b->getX();
+    double by = b->getY();
+    double cx = c->getX();
+    double cy = c->getY();
+    int xMin = round(min(ax, min(bx, cx)));
+    int xMax = round(max(ax, max(bx, cx)));
+    int yMin = round(min(ay, min(by, cy)));
+    int yMax = round(max(ay, max(by, cy)));
+    double integral = 0;
+    Segment ab(a, b);
+    Segment bc(b, c);
+    Segment ca(c, a);
+    for(int i = xMin; i <= xMax; i++) {
+        for(int j = yMin; j <= yMax; j++) {
+            // set color to 1 just to test if this method works
+            Pixel p(i, j, 1);
+            integral += p.lineIntegral(func, ab) + p.lineIntegral(func, bc) + p.lineIntegral(func, ca);
+        }
+    }
+    return integral;
 }
--- src/lineIntegral.hpp
+++ src/lineIntegral.hpp
@@ -1,7 +1,12 @@
 #ifndef line_integral_h
 #define line_integral_h
 
+#include <CImg.h>
+#include <math.h>
 #include "triangle.hpp"
+#include "pixel.hpp"
+
+using namespace cimg_library;
 
 /**
  * static class for evaluating line integral over a triangle
@@ -11,10 +16,13 @@ class LineIntegral {
     private:
         LineIntegral();
     public:
-        // static method for evaluating integral of func over triangle
+        // static method for evaluating integral of func over triangle in image img
         static double evaluate(double (*func)(double, double), Triangle *triangle);
-        // static method for evaluating integral of func over triangle with vertices *a, *b, *c
+        // static double evaluate(double (*func)(double, double), CImg<unsigned char> *img, Triangle *triangle);
+
+        // static method for evaluating integral of func over triangle with vertices *a, *b, *c in image img
         static double evaluate(double (*func)(double, double), Point *a, Point *b, Point *c);
+        // static double evaluate(double (*func)(double, double), CImg<unsigned char> *img, Point *a, Point *b, Point *c);
 };
 
 #endif
