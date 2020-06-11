35f5a72b91c883e3394ae2a811496294468d32c4
liwanlin <liwanlin@mit.edu>
2020-06-11 10:33:32 -0400

    remove unnecessary methods

--- src/lineIntegral.hpp
+++ src/lineIntegral.hpp
@@ -18,11 +18,9 @@ class LineIntegral {
     public:
         // static method for evaluating integral of func over triangle in image with pixels pixVec
         static double evaluate(double (*func)(double, double), vector<vector<Pixel>> *pixVec, Triangle *triangle);
-        // static double evaluate(double (*func)(double, double), CImg<unsigned char> *img, Triangle *triangle);
 
         // static method for evaluating integral of func over triangle with vertices *a, *b, *c in image with pixels pixVec
         static double evaluate(double (*func)(double, double), vector<vector<Pixel>> *pixVec, Point *a, Point *b, Point *c);
-        // static double evaluate(double (*func)(double, double), CImg<unsigned char> *img, Point *a, Point *b, Point *c);
 };
 
 #endif
