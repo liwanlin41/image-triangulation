04ed3b270b59f7ed8f1b63d97efc5064e26339a3
liwanlin <liwanlin@mit.edu>
2020-06-11 10:30:38 -0400

    read input from given pixels

--- src/lineIntegral.hpp
+++ src/lineIntegral.hpp
@@ -1,12 +1,12 @@
 #ifndef line_integral_h
 #define line_integral_h
 
-#include <CImg.h>
 #include <math.h>
+#include <vector>
 #include "triangle.hpp"
 #include "pixel.hpp"
 
-using namespace cimg_library;
+using namespace std;
 
 /**
  * static class for evaluating line integral over a triangle
@@ -16,12 +16,12 @@ class LineIntegral {
     private:
         LineIntegral();
     public:
-        // static method for evaluating integral of func over triangle in image img
-        static double evaluate(double (*func)(double, double), Triangle *triangle);
+        // static method for evaluating integral of func over triangle in image with pixels pixVec
+        static double evaluate(double (*func)(double, double), vector<vector<Pixel>> *pixVec, Triangle *triangle);
         // static double evaluate(double (*func)(double, double), CImg<unsigned char> *img, Triangle *triangle);
 
-        // static method for evaluating integral of func over triangle with vertices *a, *b, *c in image img
-        static double evaluate(double (*func)(double, double), Point *a, Point *b, Point *c);
+        // static method for evaluating integral of func over triangle with vertices *a, *b, *c in image with pixels pixVec
+        static double evaluate(double (*func)(double, double), vector<vector<Pixel>> *pixVec, Point *a, Point *b, Point *c);
         // static double evaluate(double (*func)(double, double), CImg<unsigned char> *img, Point *a, Point *b, Point *c);
 };
 
--- tests/lineIntegralTest.cpp
+++ tests/lineIntegralTest.cpp
@@ -5,6 +5,19 @@
 
 using namespace std;
 
+// for testing purposes, create an 1000 x 500 image all of whose pixels have color 1
+vector<vector<Pixel>> generateFakeImage() {
+    vector<vector<Pixel>> image;
+    for(int i = 0; i < 1000; i++) {
+        vector<Pixel> holder;
+        for(int j = 0; j < 500; j++) {
+            holder.push_back(Pixel(i, j, 1));
+        }
+        image.push_back(holder);
+    }
+    return image;
+}
+
 // some test functions
 
 double z(double x, double y) {
@@ -22,30 +35,33 @@ double linear(double x, double y) {
 }
 
 TEST(IntegralTest, Zero) {
+    vector<vector<Pixel>> image = generateFakeImage();
     Point a(0,0);
     Point b(10,10);
     Point c(20,30);
     Triangle triangle(&a, &b, &c);
-    ASSERT_EQ(0, LineIntegral::evaluate(z, &triangle));
-    ASSERT_EQ(0, LineIntegral::evaluate(z, &a, &b, &c));
+    ASSERT_EQ(0, LineIntegral::evaluate(z, &image, &triangle));
+    ASSERT_EQ(0, LineIntegral::evaluate(z, &image, &a, &b, &c));
 }
 
 TEST(IntegralTest, Constant) {
+    vector<vector<Pixel>> image = generateFakeImage();
     Point a(0,0);
     Point b(3,0);
     Point c(0,4);
     Triangle triangle(&a, &b, &c);
-    ASSERT_EQ(12, LineIntegral::evaluate(constant, &triangle));
-    ASSERT_EQ(12, LineIntegral::evaluate(constant, &a, &b, &c));
+    ASSERT_EQ(12, LineIntegral::evaluate(constant, &image, &triangle));
+    ASSERT_EQ(12, LineIntegral::evaluate(constant, &image, &a, &b, &c));
 }
 
 TEST(IntegralTest, Linear) {
+    vector<vector<Pixel>> image = generateFakeImage();
     Point a(0,0);
     Point b(3,0);
     Point c(0,4);
     Triangle triangle(&a, &b, &c);
-    ASSERT_EQ(30, LineIntegral::evaluate(linear, &triangle));
-    ASSERT_EQ(30, LineIntegral::evaluate(linear, &a, &b, &c));
+    ASSERT_EQ(30, LineIntegral::evaluate(linear, &image, &triangle));
+    ASSERT_EQ(30, LineIntegral::evaluate(linear, &image, &a, &b, &c));
 }
 
 int main(int argc, char **argv) {
