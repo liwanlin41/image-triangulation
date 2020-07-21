9ac86f7a850bdbace30561c6e3ea0260a729ad19
liwanlin <liwanlin@mit.edu>
2020-07-20 21:06:17 -0400

    cmake gtest

--- tests/constantTest.cpp
+++ tests/constantTest.cpp
@@ -1,9 +1,20 @@
 #include <gtest/gtest.h>
-#include "../src/constant.hpp"
+#include "../src/constant.h"
 
 using namespace std;
 
+// generate 100 x 50 bw image with black rectangle in upper left corner
+CImg<unsigned char> generateFakeImage() {
+    CImg<unsigned char> result(100, 50, 1, 1, 0);
+    unsigned char white[] = {255, 255, 255};
+    unsigned char black[] = {0,0,0};
+    result.draw_rectangle(0,0,100,50, white, 1);
+    result.draw_rectangle(0, 0, 50, 25, black, 1);
+    return result;
+}
+
 // generate 100 x 50 bw image with a black rectangle in the upper left corner
+/*
 vector<vector<Pixel>> generateFakeImage() {
     vector<vector<Pixel>> image;
     for(int i = 0; i < 100; i++) {
@@ -17,9 +28,11 @@ vector<vector<Pixel>> generateFakeImage() {
     }
     return image;
 }
+*/
 
-vector<vector<Pixel>> image = generateFakeImage();
-ConstantApprox approx(&image, 3, 0.5);
+//vector<vector<Pixel>> image = generateFakeImage();
+CImg<unsigned char> image = generateFakeImage();
+ConstantApprox approx(&image, 0.5);
 
 TEST(EnergyTest, Initial) {
     // top and left triangles are the only two
@@ -36,9 +49,4 @@ TEST(GradientTest, Syntax) {
 
 TEST(RunTest, Syntax) {
     approx.run();
-}
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
