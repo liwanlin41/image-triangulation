45889bc6ca593e156a6a0fef38c4cdd14270e347
liwanlin <liwanlin@mit.edu>
2020-08-08 10:35:29 -0400

    allow passing output file name

--- outputs/crop.cpp
+++ outputs/crop.cpp
@@ -8,19 +8,24 @@ using namespace std;
 using namespace cimg_library;
 
 int main(int argc, char* argv[]) {
-    // default image path and density
     const char *imgPath;
+    const char *outputName;
 
-    string inputPath; // to ensure non-null pointer later; find image directory
-    if (argc >= 2) {
-        inputPath = argv[1];
-        imgPath = inputPath.c_str();
-    } else {
+    string inputPath; 
+    string outputPath = "cropped.jpeg"; // default output file name
+    if(argc < 2) {
         cout << "Please choose an image to crop." << endl;
+        return 0;
     }
+    inputPath = argv[1];
+    imgPath = inputPath.c_str();
+    if (argc == 3) {
+        outputPath = argv[2];
+    }
+    outputName = outputPath.c_str();
 
     CImg<unsigned char> image(imgPath);
     CImg<unsigned char> cropped = image.autocrop();
-    cropped.save_jpeg("cropped.jpeg");
+    cropped.save_jpeg(outputName);
     return 0;
 }
